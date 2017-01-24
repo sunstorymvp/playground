c5846e4b2fffe17db0316119001327e9d6b1c9f9
Dmytro Haponov <dmytro_haponov@epam.com>
2017-01-25 01:27:31 +0200

    rename github token constant

--- src/config/apollo.js
+++ src/config/apollo.js
@@ -1,13 +1,13 @@
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 
 import { GITHUB_GRAPHQL_URL } from './constants.endpoints';
-import { GITHUB_GRAPHQL_TOKEN } from './constants.secrets';
+import { GITHUB_AUTH_TOKEN } from './constants.secrets';
 
 const networkInterface = createNetworkInterface({ uri: GITHUB_GRAPHQL_URL });
 const auth = {
   applyMiddleware(request, next) {
     request.options.headers = request.options.headers || {};
-    request.options.headers.Authorization = `Bearer ${ GITHUB_GRAPHQL_TOKEN }`;
+    request.options.headers.Authorization = `Bearer ${ GITHUB_AUTH_TOKEN }`;
     next();
   }
 };
