2057f12d0687376b380ef8269d42609bfb93a8b5
Dmytro Haponov <dmytro_haponov@epam.com>
2017-03-11 17:59:22 +0200

    setup redux dev tools

--- src/config/store.enhancer.js
+++ src/config/store.enhancer.js
@@ -1,4 +1,4 @@
-import { applyMiddleware } from 'redux';
+import { applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import { routerMiddleware } from 'react-router-redux';
@@ -17,6 +17,11 @@ const middlewares = [ promise, thunk, router, apollo ];
 
 NODE_ENV === 'development' && middlewares.push(logger);
 
-const enhancer = applyMiddleware(...middlewares);
+// eslint-disable-next-line no-underscore-dangle, no-undef
+const composeEnhancers = typeof __REDUX_DEVTOOLS_EXTENSION_COMPOSE__ === 'function' ? __REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middlewares)
+);
 
 export default enhancer;
