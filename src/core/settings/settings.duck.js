c408e027e38808bf4ea4cc7093d6487c495d07f5
Dmytro Haponov <dmytro_haponov@epam.com>
2017-10-17 01:34:03 +0300

    remove moment, lodash

--- src/core/settings/settings.duck.js
+++ src/core/settings/settings.duck.js
@@ -1,10 +1,12 @@
 import { createActions, handleActions } from 'redux-actions';
-import { merge } from 'lodash';
 
 export const actions = createActions('UPDATE_SETTINGS');
 
 const initialState = {};
 
 export const reducer = handleActions({
-  [actions.updateSettings]: (state, action) => merge({}, state, action.payload)
+  [actions.updateSettings]: (state, action) => ({
+    ...state,
+    ...action.payload
+  })
 }, initialState);
